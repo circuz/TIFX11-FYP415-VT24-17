477bff2e66a992970f511a04a20d2bb8b72d0e39
August Mint <augwik@gmail.com>
2024-03-20 22:25:54 +0100

    Fixed pandas data collection

--- computer vision/computer_vision.py
+++ computer vision/computer_vision.py
@@ -1,9 +1,12 @@
 import cv2
+import datetime
 import apriltag
 import numpy as np
 import pandas as pd
 import matplotlib.animation
 import matplotlib.pyplot as plt
+
+from functools import partial
 
 import cammands
 
@@ -13,8 +16,12 @@ try:
     capture, detector = cammands.initialize_camera(0, "tag36h11")
     crop_coords = cammands.get_crop_coords(capture, detector)
 except TypeError:
-    capture, detector = cammands.initialize_camera(1, "tag36h11")
-    crop_coords = cammands.get_crop_coords(capture, detector)
+    try:
+        capture, detector = cammands.initialize_camera(1, "tag36h11")
+        crop_coords = cammands.get_crop_coords(capture, detector)
+    except TypeError:
+        capture, detector = cammands.initialize_camera(2, "tag36h11")
+        crop_coords = cammands.get_crop_coords(capture, detector)
 
 print(crop_coords)
 
@@ -22,15 +29,18 @@ _, frame = capture.read()
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 detected_tags = detector.detect(gray)
 tagids = { tag['id'] : [] for tag in detected_tags }
-df = pd.DataFrame(tagids)
-print(df)
+dataframe = pd.DataFrame(tagids)
+print(dataframe)
 
-
-key = None
 
 def main_loop(i, df):
     print(i)
+    # Initialize each row to a bunch of Nones
+    df.loc[f'{i}_x'] = np.NaN
+    df.loc[f'{i}_y'] = np.NaN
+
     key = None
+
     if key == ord("q"):
         exit()
 
@@ -38,6 +48,11 @@ def main_loop(i, df):
 
     if key == ord("r"):
         crop_coords = cammands.get_crop_coords(capture, detector)
+
+    if ((i+1) % 50) == 0:
+        csv_name = f'{datetime.datetime.now()}.csv'
+        print(csv_name)
+        df.to_csv(csv_name)
 
     frame = raw_frame#[crop_coords]
 
@@ -52,12 +67,16 @@ def main_loop(i, df):
             pointsy = []
 
         for tag in detected_tags:
+            name = tag["id"]
+            if not (name in df.columns):
+                df.insert(0, name, np.NaN)
             xpoint = tag["center"][0]
             ypoint = tag["center"][1]
-            name = tag["id"]
+            df.loc[f'{i}_x'][name] = xpoint
+            df.loc[f'{i}_y'][name] = ypoint
             if CHEMOTAXI:
                 pointsx.append(xpoint)
-                pointsy.append(xpoint)
+                pointsy.append(ypoint)
             
             plt.scatter(pointsx, pointsy, c='white', s=100)
 
@@ -76,6 +95,8 @@ fig1, ax1 = plt.subplots()
 if CHEMOTAXI:
     ax1.set_facecolor('black')
 
-ani = matplotlib.animation.FuncAnimation(fig1, main_loop, frames=100, repeat=False, interval=50, fargs = (df))
+main_loop_with_args = partial(main_loop, df=dataframe)
+
+ani = matplotlib.animation.FuncAnimation(fig1, main_loop_with_args, frames=100, repeat=False, interval=50)
 plt.show()
 print("We're done here!")
