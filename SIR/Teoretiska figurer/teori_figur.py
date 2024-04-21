8a21d3d831d6ccfb7ea4476ca3aacec4bb5a0a4e
edvin-ketabati <edvket@gmail.com>
2024-04-21 14:38:10 +0200

    -

--- SIR/Teoretiska figurer/teori_figur.py
+++ SIR/Teoretiska figurer/teori_figur.py
@@ -7,17 +7,17 @@ plt.style.use('seaborn-v0_8')
 from scipy.integrate import solve_ivp
 
 # Parameters
-beta = 0.7 / 30  # transmission rate
-nu = 1 / 5  # recovery rate
+beta = 0.7 / 10  # transmission rate
+nu = 1 / 3  # recovery rate
 N = 30  # total population
 
 # Initial conditions
-S0 = 25
-I0 = 5
+S0 = 27
+I0 = 2
 R0 = 0
 
 # Time span
-t = np.linspace(0, 40, 1000)  # From day 0 to day 40
+t = np.linspace(0, 10, 1000) 
 
 # The SIR model differential equations.
 def sir_model(t, y):
@@ -37,8 +37,9 @@ sol = solve_ivp(sir_model, [min(t), max(t)], y0, t_eval=t)
 plt.plot(sol.t, sol.y[0], 'b', label='Mottagliga')
 plt.plot(sol.t, sol.y[1], 'r', label='Infekterade')
 plt.plot(sol.t, sol.y[2], 'g', label='Återställda')
-plt.title('SIR Modellen')
-plt.xlabel('Dagar')
+plt.title(f"SIR-simulering med $\\beta$ = {round(beta,1)} och $\\nu$ = {round(nu,1)}")
+plt.xlabel('Tid (Minuter)')
 plt.ylabel('Antal individer')
 plt.legend()
 plt.show()
+# plt.savefig("sir_teori.pgf")
